2a58d6e37900bd794fd9a9d64abbcff3441c36a5
Edbert Voo <developer@edbertvoo.com>
2019-08-20 16:56:47 -0600

    Needed to remake context

---
+++ NueraBackend/NueraBackend/Models/DTO/ItemDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace NueraBackend.Models.DTO
+{
+    public class ItemDTO
+    {
+        [JsonProperty(PropertyName ="itemName")]
+        public string ItemName { get; set; }
+
+        [JsonProperty(PropertyName = "itemName")]
+        public string ItemId { get; set; }
+
+        [JsonProperty(PropertyName = "value")]
+        public int Value { get; set; }
+
+        [JsonProperty(PropertyName = "category")]
+        public string Category { get; set; }
+    }
+}
