7631c6328af9dc6e264aab51d7e529355c5f1e44
Edbert Voo <developer@edbertvoo.com>
2019-08-21 01:28:25 -0600

    completed item creation

--- NueraBackend/NueraBackend/Controllers/Api/ItemController.cs
+++ NueraBackend/NueraBackend/Controllers/Api/ItemController.cs
@@ -1,35 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
+using Newtonsoft.Json;
+using NueraBackend.Models;
 using NueraBackend.Models.DTO;
+
 
 namespace NueraBackend.Controllers
 {
     public class ItemController : ApiController
     {
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IHttpActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok();
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
 
         // POST api/<controller>
-        public IHttpActionResult Post(ItemDTO value)
+        public IHttpActionResult Post(List<ItemDTO> itemDTO)
         {
-            return Ok("It Work");
+            NeuraBackendEntities dbContext = new NeuraBackendEntities();
+            foreach (var element in itemDTO)
+            {
+                Item newItem = new Item
+                {
+                    ItemId = element.ItemId,
+                    Name = element.Name,
+                    Category = element.Category,
+                    Value = element.Value
+                };
+
+                dbContext.Items.Add(newItem);
+            }
+
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Unable to save items");
+            }
+            
+
+
+            return Ok("Items sucessfully saved");
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody] string value)
         {
         }
 
